427f8ed03c86db834649a0da819a7df3ed2c15fd
SNatarajan <suseendran.natarajan@icf.com>
2022-07-14 13:51:47 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,3 +1,10 @@
+jte{ 
+    allow_scm_jenkinsfile = true
+    pipeline_template = "some_template"
+    permissive_initialization = false
+    reverse_library_resolution = false
+}
+
 libraries{
     maven
 }
