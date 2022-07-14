4b5dd9f2bd34a6f6a43ecece59cd5b37e8c981bc
SNatarajan <suseendran.natarajan@icf.com>
2022-07-14 11:41:21 -0400

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,3 +2,13 @@ libraries{
     maven
     ansible
 }
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
+}
